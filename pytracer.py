ff1c66ef6461fe3798300feacdc3ba850e1b79a6
Dallas-D <dallas.downing@comcast.net>
2019-07-15 17:53:49 -0400

    Some PEP8 fixes (but not all fixed)

--- pytracer.py
+++ pytracer.py
@@ -1,111 +1,111 @@
 from tracelib import *
 
 scene = Scene(
-    background_color = Vec(0.235294, 0.67451, 0.843137),
-    materials = {
+    background_color=Vec(0.235294, 0.67451, 0.843137),
+    materials={
         'glass': Material(
-            reflective = True,
-            refractive = True,
-            spec_exponent = 25,
-            diffuse_color = Vec(0.2, 0.2, 0.2),
-            Kd = 0.8,
-            Ks = 0.2,
-            ior = 1.52
+            reflective=True,
+            refractive=True,
+            spec_exponent=25,
+            diffuse_color=Vec(0.2, 0.2, 0.2),
+            Kd=0.8,
+            Ks=0.2,
+            ior=1.52
         ),
         'glossy': Material(
-            reflective = False,
-            refractive = False,
-            spec_exponent = 25,
-            diffuse_color = Vec(0.8, 0.7, 0.2),
-            Kd = 0.8,
-            Ks = 0.2,
-            ior = 1.0
+            reflective=False,
+            refractive=False,
+            spec_exponent=25,
+            diffuse_color=Vec(0.8, 0.7, 0.2),
+            Kd=0.8,
+            Ks=0.2,
+            ior=1.0
         ),
         'rubber': Material(
-            reflective = False,
-            refractive = False,
-            spec_exponent = 25,
-            diffuse_color = Vec(0.2, 0.2, 0.2),
-            Kd = 0.8,
-            Ks = 0.2,
-            ior = 1.3
+            reflective=False,
+            refractive=False,
+            spec_exponent=25,
+            diffuse_color=Vec(0.2, 0.2, 0.2),
+            Kd=0.8,
+            Ks=0.2,
+            ior=1.3
         ),
         'floor': Material(
-            reflective = False,
-            refractive = False,
-            spec_exponent = 1,
-            diffuse_color = Vec(0.8, 0.8, 0.8),
-            Kd = 0.8,
-            Ks = 0.2,
-            ior = 1.3
+            reflective=False,
+            refractive=False,
+            spec_exponent=1,
+            diffuse_color=Vec(0.8, 0.8, 0.8),
+            Kd=0.8,
+            Ks=0.2,
+            ior=1.3
         ),
         'mirror': Material(
-            reflective = True,
-            refractive = False,
-            spec_exponent = 25,
-            diffuse_color = Vec(0.1, 0.1, 0.1),
-            Kd = 1.0,
-            Ks = 0.2,
-            ior = 0.01
+            reflective=True,
+            refractive=False,
+            spec_exponent=25,
+            diffuse_color=Vec(0.1, 0.1, 0.1),
+            Kd=1.0,
+            Ks=0.2,
+            ior=0.01
         )
     },
     # x is horizontal position (right-positive)
     # y is vertical position (top-positive)
     # z is distance from camera (far-positive)
-    objs = [
+    objs=[
         Sphere(
-            pos = Vec(-1.0, 1.0, 7.0),
-            radius = 2.0,
-            material = 'glass'
+            pos=Vec(-1.0, 1.0, 7.0),
+            radius=2.0,
+            material='glass'
         ),
         Sphere(
-            pos = Vec(-0.75, -1.0, 12.0),
-            radius = 2.0,
-            material = 'glass'
+            pos=Vec(-0.75, -1.0, 12.0),
+            radius=2.0,
+            material='glass'
         ),
         Sphere(
-            pos = Vec(3.0, 0.5, 6.0),
-            radius = 1.5,
-            material = 'glass'
+            pos=Vec(3.0, 0.5, 6.0),
+            radius=1.5,
+            material='glass'
         ),
         Sphere(
-            pos = Vec(2.5, 0.0, 4.0),
-            radius = 1.0,
-            material = 'glass'
+            pos=Vec(2.5, 0.0, 4.0),
+            radius=1.0,
+            material='glass'
         ),
         Plane(
-            v0 = Vec(0, -5, 0),
-            v1 = Vec(1, -5, 0),
-            v2 = Vec(1, -5, 1),
-            material = 'floor'
+            v0=Vec(0, -5, 0),
+            v1=Vec(1, -5, 0),
+            v2=Vec(1, -5, 1),
+            material='floor'
         )
     ],
-    lights = [
+    lights=[
         Light(
-            pos = Vec(-20, 70, -20),
-            intensity = 0.5
+            pos=Vec(-20, 70, -20),
+            intensity=0.5
         ),
         Light(
-            pos = Vec(30, 50, 12),
-            intensity = 1.0
+            pos=Vec(30, 50, 12),
+            intensity=1.0
         ),
         Light(
-            pos = Vec(10, -30, 12),
-            intensity = 0.4
+            pos=Vec(10, -30, 12),
+            intensity=0.4
         )
     ]
 )
 
 camera = Camera(
-    fov = 90.0,
-    res = (960, 540),
-    pos = Vec(0, 0, 0),
-    rot = Vec(0, 0, 0)
+    fov=90.0,
+    res=(960, 540),
+    pos=Vec(0, 0, 0),
+    rot=Vec(0, 0, 0)
 )
 
 options = Options(
-    max_depth = 5,
-    proc_count = 4
+    max_depth=5,
+    proc_count=4
 )
 
 if __name__ == '__main__':
--- tracelib/primitives.py
+++ tracelib/primitives.py
@@ -1,7 +1,8 @@
-from .core import NO_INTERSECTION, min_pos, clamp, sgn
+import math
+
+from .core import NO_INTERSECTION, min_pos
 from .vector import Vec
 
-import math
 
 class Sphere:
     def __init__(self, pos, radius, material):
@@ -14,21 +15,22 @@ class Sphere:
         b = m.dot(d)
         c = m.mag2() - self.r2
 
-        if (c > 0 and b > 0):
+        if c > 0 and b > 0:
             return NO_INTERSECTION
 
         discr = b ** 2 - c
 
-        if (discr < 0):
+        if discr < 0:
             return NO_INTERSECTION
 
         t = min_pos(-b - math.sqrt(discr), -b + math.sqrt(discr))
         q = p + d * t
 
-        return (t, q)
+        return t, q
 
     def normal(self, p, q):
         return ~(q - self.pos)
+
 
 class Plane:
     def __init__(self, v0, v1, v2, material):
@@ -42,28 +44,29 @@ class Plane:
 
         n_dot_dir = n.dot(d)
 
-        if (abs(n_dot_dir) < .0001):
+        if abs(n_dot_dir) < .0001:
             return NO_INTERSECTION
 
         s = n.dot(self.v0)
         t = (n.dot(p) + s) / n_dot_dir
 
-        if (t < 0):
+        if t < 0:
             return NO_INTERSECTION
 
         q = p + d * t
 
-        return (t, q)
+        return t, q
 
     def normal(self, p, q):
         n = (self.v1 - self.v0).cross(self.v2 - self.v0)
         d = n.dot(self.v0)
         r = Vec(*n, d).dot(Vec(*p, 1))
 
-        if (r < 0):
+        if r < 0:
             n *= -1
 
         return ~n
+
 
 class Triangle:
     def __init__(self, v0, v1, v2, material):
@@ -77,30 +80,30 @@ class Triangle:
 
         n_dot_dir = n.dot(d)
 
-        if (abs(n_dot_dir) < 0.0001):
+        if abs(n_dot_dir) < 0.0001:
             return NO_INTERSECTION
 
         s = n.dot(self.v0)
         t = (n.dot(p) + s) / n_dot_dir
 
-        if (t < 0):
+        if t < 0:
             return NO_INTERSECTION
 
         q = p + d * t
 
         if ((n.dot((self.v1 - self.v0).cross(q - self.v0)) < 0) or
-            (n.dot((self.v2 - self.v1).cross(q - self.v1)) < 0) or
-            (n.dot((self.v0 - self.v2).cross(q - self.v2)) < 0)):
+                (n.dot((self.v2 - self.v1).cross(q - self.v1)) < 0) or
+                (n.dot((self.v0 - self.v2).cross(q - self.v2)) < 0)):
             return NO_INTERSECTION
 
-        return (t, q)
+        return t, q
 
     def normal(self, p, q):
         n = (self.v1 - self.v0).cross(self.v2 - self.v0)
         s = n.dot(self.v0)
         r = Vec(*n, s).dot(Vec(*p, 1))
 
-        if (r < 0):
+        if r < 0:
             n *= -1
 
         return ~n
